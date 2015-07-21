35d80803f770ca77eb405b749520988bda972427
Paul Engler <paul.engler@sjrb.ca>
2015-07-21 14:27:40 -0600

    dollar label

--- prototyping_figures.R
+++ prototyping_figures.R
@@ -9,11 +9,7 @@ p = ggplot(data=damage, aes(x=EVTYPE, y=value, fill=variable)) +
    #facet_grid(. ~ variable) +
     geom_bar(stat="identity") +
     theme(axis.text.x=element_text(angle = -90, hjust = 0, vjust = .5)) +
-<<<<<<< HEAD
     scale_y_continuous(labels=dollar)
 
-=======
-    scale_y_continuous()
->>>>>>> bdab65a27936cac1f6e318cd4ce4afd9b1dd309a
 
 print(p)
